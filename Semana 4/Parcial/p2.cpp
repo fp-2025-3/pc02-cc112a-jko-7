46445b2fb4e536022e0ddf45755bd7a65f54916e
jko-7 <julio.-2011@hotmail.com>
2026-02-07 18:35:31 -0500

    solución p1 y p2

--- Semana 4/Parcial/p2.cpp
+++ Semana 4/Parcial/p2.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
-#include <cctype>
 #include <cstring>
-#include <cstdlib>
 using namespace std;
 
-void leerCorredores (char nombres[100][100], int tiempoLlegada[], int &N){
-    cout<<"Ingrese numero de corredores: ";
-    cin>>N;     
+const int MAX_CORREDORES = 30; // Máximo 30 corredores
+const int MAX_NOMBRE = 21; // 20 caracteres + '\0'
+
+void leerCorredores (char nombres[][MAX_NOMBRE], int tiempoLlegada[], int &N){
+    do {
+        cout<<"Ingrese numero de corredores: ";
+        cin>>N; 
+
+        if (N<1 || N>MAX_CORREDORES){
+            cout<<"Error: debe ingresar entre 1 y "<<MAX_CORREDORES<<" corredores."<<endl;
+        }
+    } while (N<1 || N>MAX_CORREDORES);
+        
     for (int i=0; i<N; i++){
         cout<<"Nombre del corredor "<<i+1<<": ";
         cin>>nombres[i];
         
-        cout<<"Tiempo (en segundos): ";
-        cin>>tiempoLlegada[i];
+        do {
+            cout<<"Tiempo (en segundos): ";
+            cin>>tiempoLlegada[i];
+
+            if (tiempoLlegada[i]<=0){
+                cout<<"Error: el tiempo debe ser positivo."<<endl;
+            }
+        } while (tiempoLlegada[i]<=0);        
     }
 }
 
-void ordenarRanking (char nombres[100][100], int tiempoLlegada[], int N){
+void ordenarRanking (char nombres[][MAX_NOMBRE], int tiempoLlegada[], int N){
     for (int i=0; i<N-1; i++){
         for (int j=0; j<N-i-1; j++){
             if (tiempoLlegada[j]>tiempoLlegada[j+1]){
-                
                 swap(tiempoLlegada[j],tiempoLlegada[j+1]);  
-                swap(nombres[j],nombres[j+1]);      
+                swap(nombres[j], nombres[j+1]);
             }
         }
     }
 }       
 
-void mostrarRanking (char nombres[100][100], int tiempoLlegada[], int N){
+void mostrarRanking (char nombres[][MAX_NOMBRE], int tiempoLlegada[], int N){
     cout<<"\nRANKING FINAL:"<<endl;
-    for (int i=0; i<5 && i<N; i++){
+    for (int i=0; i<N; i++){
         cout<<i+1<<". "<<nombres[i]<<" - "<<tiempoLlegada[i]<<" segundos"<<endl;
     }
 }       
 
-void buscarNombre (char nombres[100][100], int tiempoLlegada[], int N){
-    char nombreBuscado[100];
+void buscarNombre (char nombres[][MAX_NOMBRE], int tiempoLlegada[], int N){
+    char nombreBuscado[MAX_NOMBRE];
     cout<<"\nIngrese nombre a buscar: ";
     cin>>nombreBuscado;
+
     bool encontrado=false;
     for (int i=0; i<N; i++){
         if (strcmp(nombres[i],nombreBuscado)==0){
@@ -49,20 +63,29 @@ void buscarNombre (char nombres[100][100], int tiempoLlegada[], int N){
             break;
         }
     }
+
     if (!encontrado){
         cout<<"Error: corredor no encontrado."<<endl;
     }       
 }       
 
-void tiempoRango (char nombres[100][100], int tiempoLlegada[], int N){
-    int t1,t2;
+void tiempoRango (char nombres[][MAX_NOMBRE], int tiempoLlegada[], int N){
+    int tmin,tmax;
     cout<<"\nIngrese tiempo minimo y maximo: ";
-    cin>>t1>>t2;
-    cout<<"Corredores en el rango ["<<t1<<", "<<t2<<"]:"<<endl;
+    cin>>tmin>>tmax;
+
+    cout<<"Corredores en el rango ["<<tmin<<", "<<tmax<<"]:"<<endl;
+    bool hayresultados = false;
+
     for (int i=0; i<N; i++){
-        if (tiempoLlegada[i]>=t1 && tiempoLlegada[i]<=t2){
+        if (tiempoLlegada[i]>=tmin && tiempoLlegada[i]<=tmax){
             cout<<nombres[i]<<" - "<<tiempoLlegada[i]<<" segundos"<<endl;
+            hayresultados = true;
         }
+    }
+
+    if (!hayresultados){
+        cout<<"No hay corredores en ese rango."<<endl;
     }
 }       
 
@@ -70,8 +93,8 @@ int main (){
 
     int N;
 
-    char nombres[100][100];
-    int tiempoLlegada[100];
+    char nombres[MAX_CORREDORES][MAX_NOMBRE];
+    int tiempoLlegada[MAX_CORREDORES];
 
     leerCorredores(nombres,tiempoLlegada,N);    
     ordenarRanking(nombres,tiempoLlegada,N); 
