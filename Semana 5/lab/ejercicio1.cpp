9ee66b798ee08752f35a41024b30fd3062f6f56b
jko-7 <julio.-2011@hotmail.com>
2026-02-11 15:47:29 -0500

    Ejercicio 1 y 2

--- Semana 5/lab/ejercicio1.cpp
+++ Semana 5/lab/ejercicio1.cpp
@@ -1,5 +1,46 @@
 #include <iostream>
 using namespace std;
 
+struct Estudiante
+{
+    string nombre;
+    float nota;
+};
 
-i
+
+int main (){
+
+    int n;
+    cout << "Ingrese el numero de estudiantes: ";
+    cin>>n;
+
+    cin.ignore();
+
+    Estudiante* estudiante = new Estudiante[n];
+
+    float notaMax=0;
+    int nombreMax;
+
+    for (int i=0; i<n; i++){
+        cout<<"Ingrese el nombre del estudiante "<<i+1<<": ";
+        getline(cin, estudiante[i].nombre);
+
+        cout<<"Ingrese la nota del estudiante "<<i+1<<": ";
+        cin>>estudiante[i].nota;
+
+        cin.ignore();
+    }
+
+    for (int i=0; i<n; i++){
+        if (notaMax<estudiante[i].nota){
+            notaMax=estudiante[i].nota;
+            nombreMax=i;
+        }
+    }
+
+    cout<<"El estudiante con la mayor nota es: "<<estudiante[nombreMax].nombre<<endl;
+    cout<<"Nota: "<<notaMax<<endl;
+
+
+    return 0;
+}
--- Semana 5/lab/ejercicio2.cpp
+++ Semana 5/lab/ejercicio2.cpp
@@ -42,3 +42,54 @@ int main (){
 
     return 0;
 }
+
+// GENERALIZANDO EL PROBLEMA 
+
+/*
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+struct Punto {
+    int x, y, z;
+};
+
+int main() {
+    // Diccionario de ordinales para los primeros puntos
+    string etiquetas[] = {"primer", "segundo", "tercer", "cuarto", "quinto"};
+    
+    int n;
+    cout << "En este ejemplo usaremos maximo 5 puntos. ¿Cuantos desea? ";
+    cin >> n;
+
+    Punto* puntos = new Punto[n];
+
+    for (int i = 0; i < n; i++) {
+        // Si el índice está dentro de nuestro arreglo de etiquetas, usamos la palabra.
+        // Si no, usamos el número normal (ej: "punto 6").
+        if (i < 5) {
+            cout << "\nIngrese las coordenadas del " << etiquetas[i] << " punto:" << endl;
+        } else {
+            cout << "\nIngrese las coordenadas del punto " << i + 1 << ":" << endl;
+        }
+
+        cout << "X: "; cin >> puntos[i].x;
+        cout << "Y: "; cin >> puntos[i].y;
+        cout << "Z: "; cin >> puntos[i].z;
+    }
+
+    // Cálculo de distancia entre el primer y segundo punto (como en tu código original)
+    if (n >= 2) {
+        float distancia = sqrt(pow((puntos[1].x - puntos[0].x), 2) + 
+                               pow((puntos[1].y - puntos[0].y), 2) + 
+                               pow((puntos[1].z - puntos[0].z), 2));
+        cout << "\nLa distancia entre el primer y segundo punto es: " << distancia << endl;
+    }
+
+    delete[] puntos;
+    return 0;
+}
+
+*/
