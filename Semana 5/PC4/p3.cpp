cfcf134672c4e3bc4877a91f2478ce59d3c87c32
jko-7 <julio.-2011@hotmail.com>
2026-02-15 11:09:25 -0500

    Modificando la función crearInventario y la salida de busqueda de producto

--- Semana 5/PC4/p3.cpp
+++ Semana 5/PC4/p3.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
-#include <string>
-#include <ctime>
-#include <cstdlib>
-#include <cctype>
 #include <cstring>
-#include <iomanip>
-#include <cmath>
 using namespace std;
 
 struct Producto
 {
     int codigo;
     char* nombre;
-    double precio;  // cadena dinámica
+    double precio;  
     int stock;
 };
 
@@ -29,23 +23,36 @@ Producto crearProducto(int codigo, const char* nombre, double precio, int stock)
     p.stock = stock;
 
     return p;           
-
 }
 
 Producto* crearInventario(int n){
     
     Producto* inventario = new Producto[n];
 
-    for (int i=0; i<n; i++){
-        inventario[i] = crearProducto(i+1, "Producto", 10.0, 5);
+    inventario[0] = crearProducto(100, "Teclado", 10.5, 5);
+    inventario[1] = crearProducto(101, "Mouse", 21, 10);
+    inventario[2] = crearProducto(102, "Monitor", 31.5, 15);
+    inventario[3] = crearProducto(103, "Laptop", 42, 20);
+    inventario[4] = crearProducto(104, "Impresora", 52.5, 25);
+
+    /*
+    Otra forma de ingresar los datos
+
+    int codigos[] = {100, 101, 102, 103, 104};
+    const char* nombres[] = {"Teclado", "Mouse", "Monitor", "Laptop", "Impresora"};
+    double precios[] = {10.5, 21, 31.5, 42, 52.5};
+    int stocks[] = {5, 10, 15, 20, 25};
+    
+    for(int i = 0; i < n; i++){
+        inventario[i] = crearProducto(codigos[i], nombres[i], precios[i], stocks[i]);
     }
+    */
 
     return inventario;
-
 }
 
 Producto* buscarProducto(Producto* inventario, int n, int codigoBuscado){
-
+    
     for (int i=0; i<n; i++){
         if (inventario[i].codigo == codigoBuscado){
             return &inventario[i];
@@ -55,26 +62,40 @@ Producto* buscarProducto(Producto* inventario, int n, int codigoBuscado){
 }
 
 void liberarInventario(Producto* inventario, int n){
-    // liberar memoria
 
     for (int i=0; i<n; i++){
         delete[] inventario[i].nombre;
     }
     delete[] inventario;    
-
-
 }
 
-
 int main (){
-    int n = 3;
+    int n = 5;
     Producto* inventario = crearInventario(n);      
 
+    cout<<"\tINVENTARIO"<<endl;
+
     for (int i=0; i<n; i++){
-        cout << "Codigo: " << inventario[i].codigo << ", Nombre: " << inventario[i].nombre << ", Precio: " << inventario[i].precio << ", Stock: " << inventario[i].stock << endl;
+        cout<<"Codigo: "<<inventario[i].codigo 
+            <<" | Nombre: "<<inventario[i].nombre 
+            <<" | Precio: "<<inventario[i].precio 
+            <<" | Stock: "<<inventario[i].stock << endl;
     }   
 
+    int codigoBuscar = 102;
+    Producto* encontrado = buscarProducto(inventario, n, codigoBuscar);
 
+    cout<<"\nBuscando producto con codigo "<<codigoBuscar<<"..."<<endl;
+
+    if (encontrado!=nullptr){
+        cout<<"Producto encontrado: "<<encontrado->nombre
+            <<" | Precio: "<<encontrado->precio<<endl;
+    } else {
+        cout<<"Producto no encontrado."<<endl;
+    }
+    
+    // Liberar memoria
+    liberarInventario(inventario, n);
 
     return 0;
 }
